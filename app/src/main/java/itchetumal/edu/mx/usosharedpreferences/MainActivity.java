be5b9e39e74b6fc85127bbf1ba11bf1856bf357a
lab <lab.computo.itch@gmail.com>
2016-11-18 15:54:21 -0500

    Initial commit

---
+++ app/src/main/java/itchetumal/edu/mx/usosharedpreferences/MainActivity.java
@@ -0,0 +1,88 @@
+package itchetumal.edu.mx.usosharedpreferences;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
+    private EditText et2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Shared Preferences
+        et1=(EditText)findViewById(R.id.et1);
+        SharedPreferences prefe=getSharedPreferences("datos", Context.MODE_PRIVATE);
+        et1.setText(prefe.getString("mail","@mail.com"));
+
+        //Lectura de archivo
+
+        et2=(EditText)findViewById(R.id.et2);
+        String[] archivos = fileList();
+
+        if (existe(archivos, "notas.txt"))
+            try {
+                InputStreamReader archivo = new InputStreamReader(
+                        openFileInput("notas.txt"));
+                BufferedReader br = new BufferedReader(archivo);
+                String linea = br.readLine();
+                String todo = "";
+                while (linea != null) {
+                    todo = todo + linea + "\n";
+                    linea = br.readLine();
+                }
+                br.close();
+                archivo.close();
+                et2.setText(todo);
+            } catch (IOException e) {
+            }
+    }
+
+    //Método para SharedPreferences
+    public void ejecutar(View v) {
+        SharedPreferences preferencias=getSharedPreferences("datos",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=preferencias.edit();
+        editor.putString("mail", et1.getText().toString());
+        editor.commit();
+        finish();
+    }
+
+    //Métodos para archivos
+    private boolean existe(String[] archivos, String archbusca) {
+        for (int f = 0; f < archivos.length; f++)
+            if (archbusca.equals(archivos[f]))
+                return true;
+        return false;
+    }
+
+    public void grabar(View v) {
+        try {
+            OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput(
+                    "notas.txt", Activity.MODE_PRIVATE));
+            archivo.write(et2.getText().toString());
+            archivo.flush();
+            archivo.close();
+        } catch (IOException e) {
+        }
+        Toast t = Toast.makeText(this, "Los datos fueron grabados",
+                Toast.LENGTH_SHORT);
+        t.show();
+        finish();
+    }
+
+
+}
